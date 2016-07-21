a215efe4f98a402a9bd622011dd4749bc4c025a9
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-07-21 10:56:08 +0100

    Add neighbour methods to WalkGraphs

--- madansi/WalkGraphs.py
+++ madansi/WalkGraphs.py
@@ -3,10 +3,6 @@ from madansi.BlastHit import BlastHit
 from madansi.GenePresent import GenePresent
 from madansi.DepthFirstSearch import DepthFirstSearch
 
-#First find a contig that is marked as present in the graph file.
-#Then look at all nodes that are neighbors of this node and see if there is a contig in the same or a different sequence (should be marked as being present in the lookup table) here. If it is in the same sequence - which we will know from looking at the filtered fasta file and the sample name- so should be the second column given that we have swapped the columns initially.
-#If a second sample has not been found then we would need to extend our search to two neighbours away and continue in that way. However, if a sample has been found then we need to go to the last present contig in that sequence and continue the search from there.
-
 class WalkGraphs(object):
     
     def __init__(self,graphfile,filteredfile):
@@ -19,13 +15,15 @@ class WalkGraphs(object):
             g=nx.Graph(nx.drawing.nx_pydot.read_dot(self.graphfile))
             return g
         except IOError:
-            raise Error("Error opening this file")
-
+            raise IOError("Error opening this file")
     
-# Choose starting sequence and will need to iterate through all of the sequences until finished.
-    sequence_list=[]
-    visited=[]       
-         
+    def open_filtered_file(self):
+        """Opens the fitlered file for searching"""
+        try:
+            f = open(self.filteredfile,'r')
+            return f
+        except IOError:
+            raise IOError("Error opening this file") 
         
     def create_subgraph(self):
         """Creates a subgraph with nodes representing the genes that are marked as present"""
@@ -36,27 +34,75 @@ class WalkGraphs(object):
     def starting_gene(self):
         """Chooses a starting gene and returns it"""
         h = self.create_subgraph()
-        start_gene = h.node[0]
+        for node in nx.nodes_iter(h):
+            start_gene = node
+            break
         return start_gene
     
-    def find_sequence(self, gene):
+    def find_contig(self, gene):
         """Given a gene will find out the sequence that it is in"""
-        f = open(self.filteredfile,'r')
+        f = self.open_filtered_file()
         for line in f:
             l = line.rstrip().split('\t')
             if l[0] == gene:
                 return l[1]
-         
-         
+    
+    def construct_contig_list(self):
+        """Constructs a list that will contain all of the names of the contigs"""
+        contig_list =[]
+        f = self.open_filtered_file()
+        for line in f:
+            l = line.rstrip().split('\t')
+            if l[1] not in contig_list:
+               contig_list.append(l[1])
+        return contig_list
+        
+    def find_neighbors_on_contig(self, gene):
+        """Given a gene in the graph checks whether it is marked and if it is returns a list of all its
+        neighbors also on the graph"""
+        g = self.open_graph_file()
+        h = self.create_subgraph()
+        if h.node[gene]:
+            contig = self.find_contig(gene)
+            contig_neighbor_list = []
+            for neighbor in g.neighbors(gene):
+                if g.node[neighbor]['present'] and self.find_contig(neighbor) == contig:
+                    contig_neighbor_list.append(neighbor)
+        else:
+            raise KeyError('Given gene not marked as present')
+        return contig_neighbor_list
+    
+    def start_and_ends_contigs(self, gene):
+        """Finds the genes at either end of the contig, defined from a marked gene on it, where the ends
+         are defined to be based upon where the ends of the contig are marked on the graph"""
+        contig_neighbor_list = self.find_neighbors_on_contig()
+     
+    def ordering_contigs(self):
+        """Puts the contigs in order and orientates them as necessary"""
+        visited = []
+        contig_list = self.construct_contig_list()
+        start_gene = self.starting_gene()
+        start_contig = self.find_contig(start_gene)
+        visited.append(start_contig)
+        while set(visited) != set(contig_list):
+            
+        
+        
+#First find a contig that is marked as present in the graph file.
+#Then look at all nodes that are neighbors of this node and see if there is a contig in the same or a different sequence (should be marked as being present in the lookup table) here. If it is in the same sequence - which we will know from looking at the filtered fasta file and the contig name- so should be the second column given that we have swapped the columns initially.
+#If a second contig has not been found then we would need to extend our search to two neighbours away and continue in that way. However, if a contig has been found then we need to go to the last present contig in that sequence and continue the search from there.      
+   
 #Add node attributes to make sure that the genes are actually present.        
 
-#Create subgraph with all the genes present in the sample
+#Create subgraph with all the genes present in the contig
 
 #Work with this graph to find an initial gene to start with
 
-#Make a note of the sample that it is from- need to relate this back to the filtered file and should take the second column of the row where this gene is first. 
+#Make a note of the contig that it is from- need to relate this back to the filtered file and should take the second column of the row where this gene is first. 
 #Open the filtered file for reading:
 
 #Iterate through the filtered file to find the record that includes the relevant gene and return the second entry of this row
+
+#Starting with the chosen gene will want to look through the neighbours to see if we can find a gene that is marked and belongs to a different contig.
 
     
--- madansi/tests/test_WalkGraphs.py
+++ madansi/tests/test_WalkGraphs.py
@@ -7,19 +7,48 @@ import os
 class TestWalkGraphs(unittest.TestCase):
     
     def test__init__(self):
+        """Tests that the class is correctly initialised"""
         wg = WalkGraphs('ab','cd')
         self.assertTrue(wg.graphfile)
         self.assertTrue(wg.filteredfile)
     
     def test_open_graph_file(self):
+        """Tests that the graph dot file opens correctly"""
         wg = WalkGraphs('madansi/tests/data/graph_3_nodes.dot', 'cd')
         g = wg.open_graph_file()
         self.assertTrue(g)
         
+    def test_open_filtered_file(self):
+        """Tests that the file with filtered data opens correctly"""
+        wg = WalkGraphs('ab', 'madansi/tests/data/gene_present_unittest')
+        f = wg.open_filtered_file()
+        self.assertTrue(f)
+        
 #This test doesn't work because the former graph produced has the data as a number while the latter turns it into a string
          
   #  def test_create_subgraph(self):
+  #      """Tests that a subgraph with the correct data attributes is created"""
   #      wg = WalkGraphs('madansi/tests/data/graph_3_nodes.dot','madansi/tests/data/gene_present_unittest')
   #      g = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/graph_3_nodes_attributes.dot'))
   #      h = wg.create_subgraph()
   #      self.assertCountEqual(h.nodes(data=True),g.nodes(data=True))
+
+    def test_starting_gene(self):
+        """Tests that a starting gene is chosen and that it is a node in the subgraph"""
+        wg = WalkGraphs('madansi/tests/data/graph_3_nodes.dot','madansi/tests/data/gene_present_unittest')
+        h = wg.create_subgraph()
+        start_gene= wg.starting_gene()
+        self.assertTrue(h.node[start_gene])
+        
+    def test_find_contig(self):
+        """Tests that the contig that the gene is in is correctly identified"""
+        wg = WalkGraphs('madansi/tests/data/graph_3_nodes.dot','madansi/tests/data/gene_present_unittest')
+        contig = wg.find_contig('Contig3')
+        self.assertEqual(contig, '7.23.B265.9.cap3_contig')
+    
+    def test_construct_contig_list(self):
+        """Tests that a list of all the possible contig sequences are included in the list"""
+        wg = WalkGraphs('ab', 'madansi/tests/data/gene_present_unittest')
+        contig_list = wg.construct_contig_list()
+        expected_list = ['7.23.B265.9.cap3_contig', 'Sample3', 'Sample4']
+        self.assertCountEqual(contig_list, expected_list)
