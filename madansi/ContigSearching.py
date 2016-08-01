3257decdfca3951704cd4ac3a6551c6f821452b2
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-01 14:27:03 +0100

    Updated ContigSearching to stop when connection has been found

--- madansi/ContigSearching.py
+++ madansi/ContigSearching.py
@@ -9,37 +9,42 @@ class ContigSearching(object):
         self.finished_contigs = set()
     
     def contig_expansion(self):
-        for sequence_name, genes in self.gene_detector.contigs:
-            gene_names = [gene for gene in genes] 
+        for sequence_name, genes in self.gene_detector.contigs.items():
+            gene_names = [gene for gene in genes.gene_objects] 
             self.set_expansion(gene_names, sequence_name)
         
     def neighbourhood_expansion(self):
-        for sequence_name, genes in self.genes_in_contig_radius:
+        for sequence_name, genes in self.genes_in_contig_radius.items():
             self.set_expansion(genes, sequence_name)
             
     def set_expansion(self, gene_names, sequence_name):
-        neighbouring_nodes = NeighboursOfNodes(self.filtered_graph.graph).find_neighbours(gene_names)
+        neighbouring_nodes = NeighboursOfNodes(self.filtered_graph).find_neighbours(gene_names)
         self.genes_in_contig_radius[sequence_name] = gene_names + neighbouring_nodes
         if neighbouring_nodes == []:
             if sequence_name not in self.finished_contigs:
                 self.finished_contigs.add(sequence_name)
-        return
+        return self
         
     def expand_all_contigs(self):
         iteration_count = 1
         self.contig_expansion()
-        self.check_intersections(itereation_count)
-        while set([sequence_name for sequence_name in self.gene_detector.contigs]) - self.finished_contigs == set():
+        self.check_intersections(iteration_count)    
+        while set([sequence_name for sequence_name in self.gene_detector.contigs]) - self.finished_contigs != set():
             iteration_count += 1
+            print(iteration_count)
             self.neighbourhood_expansion()
             self.check_intersections(iteration_count)      
     
     def check_intersections(self, iteration_count):
-        for sequence_name_1, gene_names_1 in self.genes_in_contig_radius:
-            for sequence_name_2, gene_names_2 in self.genes_in_contig_radius:
+        for sequence_name_1, gene_names_1 in self.genes_in_contig_radius.items():
+            for sequence_name_2, gene_names_2 in self.genes_in_contig_radius.items():
+                if gene_names_1 == [] or gene_names_2 == []:
+                    continue
                 if sequence_name_1 == sequence_name_2:
                     continue
-                if [list(filter(lambda x: x in gene_names_1, sublist) for sublist in gene_names_2)] != []:
-                    self.neighbouring_contigs.append((sequence_name_1, sequence_name_2, iteration_count))
+                if set(gene_names_1) & set(gene_names_2) != set():
+                    if not any( (sequence_name_1, sequence_name_2) == (entry[0], entry[1]) or (sequence_name_1, sequence_name_2) == (entry[1], entry[0]) for entry in self.neighbouring_contigs):
+                        self.neighbouring_contigs.append((sequence_name_1, sequence_name_2,iteration_count))
+        return self
     
                 
--- madansi/tests/test_ContigSearching.py
+++ madansi/tests/test_ContigSearching.py
@@ -5,16 +5,51 @@ import networkx as nx
 
 class TestContigSearching(unittest.TestCase):
     
-    def test_initialisation(self):
+    #def test_initialisation(self):
+    #    gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/test_blast_hits')
+    #    filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/graph_3_nodes.dot'))
+    #    contig_searching = ContigSearching(gene_detector, filtered_graph)
+    #    self.assertDictEqual(contig_searching.genes_in_contig_radius, {})
+    #    self.assertCountEqual(contig_searching.neighbouring_contigs, [])
+    #    self.assertEqual(contig_searching.finished_contigs, set())
+    #    
+    #def test_set_expansion_one_contig_isolated(self):
+    #    gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/test_blast_hits')
+    #    filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/graph_3_nodes.dot'))
+    #    contig_searching = ContigSearching(gene_detector, filtered_graph)
+    #    self.assertEqual( contig_searching.set_expansion(['gene1','gene2','gene3'], 'Contig1').finished_contigs, set(['Contig1']))
+    #    
+    #def test_set_expansion_one_contig_not_isolated(self):
+    #    gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/test_blast_hits')
+    #    filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/test_graph.dot'))
+    #    contig_searching = ContigSearching(gene_detector, filtered_graph)
+    #    self.assertEqual( contig_searching.set_expansion(['gene1','gene2','gene3'], 'Contig1').finished_contigs, set())
+    #
+    #def test_expand_all_contigs_two_neighbouring_contigs(self):
+    #    gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/four_blast_hits')
+    #    filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/graph_4_nodes.dot'))
+    #    contig_searching = ContigSearching(gene_detector, filtered_graph)
+    #    contig_searching.expand_all_contigs()
+    #    contig_neighbourhoods = contig_searching.neighbouring_contigs
+    #    self.assertTrue(contig_neighbourhoods == [('Contig1', 'Contig2', 1)] or \
+    #    contig_neighbourhoods == [('Contig2', 'Contig1', 1)])
+    
+    def test_expand_all_contigs_two_contigs_further_separated(self):
         gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/test_blast_hits')
-        filtered_graph = 'madansi/tests/data/test_graph.dot'
+        filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/test_graph.dot'))
         contig_searching = ContigSearching(gene_detector, filtered_graph)
-        self.assertDictEqual(contig_searching.genes_in_contig_radius, {})
-        self.assertCountEqual(contig_searching.neighbouring_contigs, [])
-        self.assertEqual(contig_searching.finished_contigs, set())
+        contig_searching.expand_all_contigs()
+        contig_neighbourhoods = contig_searching.neighbouring_contigs
+        possible_expected_lists = [\
+            sorted([('Contig1', 'Contig2', 1),('Contig1', 'Contig3', 1),('Contig2', 'Contig3', 3)]),\
+            sorted([('Contig2', 'Contig1', 1),('Contig1', 'Contig3', 1),('Contig2', 'Contig3', 3)]),\
+            sorted([('Contig1', 'Contig2', 1),('Contig3', 'Contig1', 1),('Contig2', 'Contig3', 3)]),\
+            sorted([('Contig1', 'Contig2', 1),('Contig1', 'Contig3', 1),('Contig3', 'Contig2', 3)]),\
+            sorted([('Contig2', 'Contig1', 1),('Contig3', 'Contig1', 1),('Contig2', 'Contig3', 3)]),\
+            sorted([('Contig2', 'Contig1', 1),('Contig1', 'Contig3', 1),('Contig3', 'Contig2', 3)]),\
+            sorted([('Contig1', 'Contig2', 1),('Contig3', 'Contig1', 1),('Contig3', 'Contig2', 3)]),\
+            sorted([('Contig2', 'Contig1', 1),('Contig3', 'Contig1', 1),('Contig3', 'Contig2', 3)])]
+        print(contig_neighbourhoods)
+        self.assertTrue(sorted(contig_neighbourhoods) in possible_expected_lists)
         
-    def test_set_expansion_one_contig(self):
-        gene_detector = GeneDetector('madansi/tests/data/assembly.fa', 'madansi/tests/data/test_blast_hits')
-        filtered_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/graph_3_nodes.dot'))
-        contig_searching = ContigSearching(gene_detector, filtered_graph)
-        self.assertEqual(contig_searching.set_expansion(['gene1','gene2','gene3'], 'Contig1').finished_contigs, set('Contig1'))
+        
