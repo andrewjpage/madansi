39517c81ff3b5e361f53d8582db3ffec89690edd
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-02 11:30:24 +0100

    Filtering the blast output before using and filtering the graph to remove some nodes with too high a degree

--- madansi/tests/test_GraphParser.py
+++ madansi/tests/test_GraphParser.py
@@ -1,10 +1,22 @@
 import unittest
 from madansi.GraphParser import GraphParser
+import networkx as nx
 
 class TestGraphParse(unittest.TestCase):
     
     def test_initialisation(self):
         graph_parser = GraphParser('madansi/tests/data/test_graph.dot')
-        self.assertTrue(graph_parser.graphfile)
+        self.assertTrue(graph_parser.graph_file)
         self.assertTrue(graph_parser.graph)
         
+    def test_filter(self):
+        graph_parser = GraphParser('madansi/tests/data/unfiltered_graph.dot')
+        filtered_graph = graph_parser.filter_graph(graph_parser.open_graph_file())
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/filtered_graph.dot'))
+        self.assertTrue(nx.is_isomorphic(filtered_graph, expected_graph))
+    
+    def test_filter_no_change(self):
+        graph_parser = GraphParser('madansi/tests/data/test_graph.dot')
+        filtered_graph = graph_parser.filter_graph(graph_parser.open_graph_file())
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/test_graph.dot'))
+        self.assertTrue(nx.is_isomorphic(filtered_graph, expected_graph))
--- scripts/ordering_contigs.py
+++ scripts/ordering_contigs.py
@@ -6,11 +6,13 @@ from madansi.ContigSearching import ContigSearching
 from madansi.GeneDetector import GeneDetector
 from madansi.GraphParser import GraphParser
 from madansi.ContigGraph import ContigGraph
+from madansi.FilterBlastComparison import FilterBlastComparison
 
 parser = argparse.ArgumentParser(description = 'Script to take in the input assembly file, blast hits file and graph file and output a list of how each of the \
 contigs are connected to each other and the number of iterations it takes to get between the two.')
 parser.add_argument('input_assembly_file', help = 'Path to the input assembly file.', type = str)
 parser.add_argument('blast_hits_file', help = 'Path to the blast hits file.', type = str)
+parser.add_argument('filtered_blast_hits_file', help = 'Path to the output filtered blast hits file.', type = str)
 parser.add_argument('graph_file', help = 'Path to the inout graph file.', type = str)
 parser.add_argument('output_contig_graph_file', help = 'Path to the output contig graph file. This should be a dot file.', type = str)
 args = parser.parse_args()
@@ -19,7 +21,10 @@ args = parser.parse_args()
 graph_parser = GraphParser(args.graph_file)
 filtered_graph = graph_parser.graph
 
-gene_detector = GeneDetector(args.input_assembly_file, args.blast_hits_file)
+filtered_blast_hits_file = FilterBlastComparison(args.blast_hits_file, args.filtered_blast_hits_file,  bitscore=200)
+filtered_blast_hits_file.filter()
+
+gene_detector = GeneDetector(args.input_assembly_file, args.filtered_blast_hits_file)
 gene_detector.contigs_to_genes()
 
 contig_searching = ContigSearching(gene_detector, filtered_graph)
@@ -27,6 +32,4 @@ contig_searching.expand_all_contigs()
 
 contig_graph = ContigGraph(contig_searching.neighbouring_contigs, args.output_contig_graph_file)
 contig_graph.create_contig_subgraph()
-contig_graph.output_contig_graph()
-
 
