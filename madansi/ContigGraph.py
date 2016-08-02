f667410bd03779ccd07188d856e7afbaed2a43d2
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-02 10:47:53 +0100

    Created ContigGraph class

---
+++ madansi/ContigGraph.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+class ContigGraph(object):
+    """Given a list of tuples of pairs of contigs together with the distance between them, will generate a weighted graph showing all\
+    of these connections together with the distance between the two contigs."""
+    
+    def __init__(self, neighbouring_contigs, output_contig_graph):
+        self.neighbouring_contigs = neighbouring_contigs
+        self.contig_graph = nx.Graph()
+        self.output_contig_graph = output_contig_graph
+    
+    def create_contig_subgraph(self):
+        for entry in self.neighbouring_contigs:
+            self.contig_graph.add_nodes_from([entry[0], entry[1]])
+            self.contig_graph.add_edge(entry[0],entry[1], weight = entry[2])
+        nx.drawing.nx_pydot.write_dot(self.contig_graph, self.output_contig_graph) #Should put this into a second method
+        return self.contig_graph
+    
+    def output_contig_graph(self): 
+        nx.drawing.nx_pydot.write_dot(self.contig_graph, self.output_contig_graph)
+        return
---
+++ madansi/tests/test_ContigGraph.py
@@ -0,0 +1,58 @@
+import unittest
+from madansi.ContigGraph import ContigGraph
+import os
+import networkx as nx
+import filecmp
+
+class TestContigGraph(unittest.TestCase):
+	
+    def test__init__(self):
+        my_list = []
+        my_contig_graph = ContigGraph(my_list,'output')
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/empty_graph.dot'))
+        self.assertTrue(nx.is_isomorphic(my_contig_graph.contig_graph, expected_graph))
+        
+    def test_empty_list(self):
+        my_list = []
+        my_contig_graph = ContigGraph(my_list,'output')
+        my_contig_graph.create_contig_subgraph()
+        my_graph = my_contig_graph.contig_graph
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/empty_graph.dot'))
+        self.assertTrue(nx.is_isomorphic(my_graph, expected_graph))
+        os.unlink('output')
+    
+    def test_one_entry(self):
+        my_list = [('Contig1', 'Contig2', 3)]
+        my_contig_graph = ContigGraph(my_list,'output')
+        my_contig_graph.create_contig_subgraph()
+        my_graph = my_contig_graph.contig_graph
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/contig_graph_one_entry.dot'))
+        self.assertTrue(nx.is_isomorphic(my_graph, expected_graph))
+        os.unlink('output')
+    
+    def test_three_entries(self):
+        my_list = [('Contig1', 'Contig2',1), ('Contig2', 'Contig3',2), ('Contig3', 'Contig4',2)]
+        my_contig_graph = ContigGraph(my_list,'output')
+        my_contig_graph.create_contig_subgraph()
+        my_graph = my_contig_graph.contig_graph
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/contig_graph_three_entries.dot'))
+        self.assertTrue(nx.is_isomorphic(my_graph, expected_graph))
+        os.unlink('output')
+    
+    def test_five_entries(self):
+        my_list = [('Contig1', 'Contig2', 2), ('Contig3', 'Contig1',3), ('Contig3','Contig4',1), ('Contig4', 'Contig1', 4), ('Contig2', 'Contig5', 1)]
+        my_contig_graph = ContigGraph(my_list,'output')
+        my_contig_graph.create_contig_subgraph()
+        my_graph = my_contig_graph.contig_graph
+        expected_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/contig_graph_five_entries.dot'))
+        self.assertTrue(nx.is_isomorphic(my_graph, expected_graph))
+        os.unlink('output')
+    
+    def test_output_contig_graph(self):
+        my_list = []
+        my_contig_graph = ContigGraph(my_list,'output')
+        my_contig_graph.create_contig_subgraph()
+        
+        self.assertTrue(filecmp.cmp('output', 'madansi/tests/data/empty_graph.dot'))
+        os.unlink('output')
+        
