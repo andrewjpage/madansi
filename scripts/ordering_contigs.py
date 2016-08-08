193ec00944ec9bdc5a74d3dde6fa7962d538ccfc
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-08 11:37:16 +0100

    Including refinement of graphs into the script

--- scripts/ordering_contigs.py
+++ scripts/ordering_contigs.py
@@ -10,6 +10,7 @@ from madansi.ContigGraph import ContigGraph
 from madansi.FilterBlastComparison import FilterBlastComparison
 from madansi.ContigSpanningTree import ContigSpanningTree
 from madansi.UnusedContigs import UnusedContigs
+from madansi.RefineContigNeighbours import RefineContigNeighbours
 
 parser = argparse.ArgumentParser(description = 'Script to take in the input assembly file, blast hits file and graph file and output a list of how each of the \
 contigs are connected to each other and the number of iterations it takes to get between the two.')
@@ -23,11 +24,12 @@ parser.add_argument('output_contig_file', help = 'Path to the file containing th
 args = parser.parse_args()
 
 
-graph_parser = GraphParser(args.graph_file)
-filtered_graph = graph_parser.graph
 
 filtered_blast_hits_file = FilterBlastComparison(args.blast_hits_file, args.filtered_blast_hits_file,  bit_score=200)
 filtered_blast_hits_file.filter()
+
+graph_parser = GraphParser(args.graph_file)
+filtered_graph = graph_parser.graph
 
 gene_detector = GeneDetector(args.input_assembly_file, args.filtered_blast_hits_file)
 gene_detector.contigs_to_genes()
@@ -39,10 +41,14 @@ unused_contigs.output_unused_contigs()
 contig_searching = ContigSearching(gene_detector, filtered_graph)
 contig_searching.expand_all_contigs()
 
+#refine_neighbouring_contigs = RefineContigNeighbours(contig_searching.neighbouring_contigs, filtered_graph, args.filtered_blast_hits_file)
+#refine_neighbouring_contigs.refine_contig_neighbours()
+
 contig_graph = ContigGraph(contig_searching.neighbouring_contigs, args.output_contig_graph_file)
 contig_graph.create_contig_subgraph()
 contig_graph.output_contig_graph()
 
-contig_spanning_tree = ContigSpanningTree(contig_graph.contig_graph, args.output_spanning_tree)
-contig_spanning_tree.construct_spanning_tree()
+#contig_spanning_tree = ContigSpanningTree(contig_graph.contig_graph, args.output_spanning_tree)
+#contig_spanning_tree.construct_spanning_tree()
 
+#refine_neighbouring_contigs.refined_neighbouring_contigs
