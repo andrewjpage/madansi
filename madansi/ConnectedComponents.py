8a2eda7112776db3db41a90c3cde4c646d46c555
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-09 13:43:01 +0100

    Added dictionary relating the contains to the component that they are on

--- madansi/ConnectedComponents.py
+++ madansi/ConnectedComponents.py
@@ -3,18 +3,38 @@ from madansi.Component import Component
 
 class ConnectedComponents(object):
     
-    def __init__(self, refined_graph):
+    def __init__(self, refined_graph, unrefined_graph):
         self.refined_graph = refined_graph
-        self.components_dictionary = {}
+        self.unrefined_graph = unrefined_graph
+        self.component_list = self.connected_component_list()
     
-    def connected_components(self): #At some point maybe think about checking that the components do not have cycles
+    def connected_component_list(self):
         component_list = sorted(nx.connected_components(self.refined_graph), key = len, reverse = True)
-        for component in component_list:
+        return component_list
+    
+    def create_component_dictionary(self): #At some point maybe think about checking that the components do not have cycles
+        """Creates a dictionary including an index for each connected component in the refined graph and the component object"""
+        components_dictionary = {}
+        for component in self.component_list:
             edge_list = sorted([tuple(sorted(edge)) for edge in self.refined_graph.edges(component)])
             if len(component)==1:
                 ends = component
             else:
                 ends = [contig for contig in component if self.refined_graph.degree(contig)==1]
-            self.components_dictionary[component_list.index(component)] = Component(component_list.index(component), component,\
+            components_dictionary[self.component_list.index(component)] = Component(self.component_list.index(component), component,\
                                                                                  ends, edge_list)
-        return self.components_dictionary
+        return components_dictionary
+    
+    def create_contigs_to_components_dictionary(self):
+        """Creates a dictionary that relates the contig to the component that it is on if it lies on the refined graph and gives None if the contig is not 
+        present in the refined graph"""
+        contigs_to_components = {}
+        for component in self.component_list:
+            component_index = self.component_list.index(component)
+            for contig in component:
+                contigs_to_components[contig] = component_index
+        for contig in self.unrefined_graph.nodes():
+            if contig not in contigs_to_components:
+                contigs_to_components[contig] = None        
+        return contigs_to_components
+                
--- madansi/tests/test_ConnectedComponents.py
+++ madansi/tests/test_ConnectedComponents.py
@@ -5,24 +5,19 @@ import unittest
 
 class TestConnectedComponents(unittest.TestCase):
     
-    
-    def test_intitialisation(self):
-        components = ConnectedComponents('')
-        self.assertEqual(components.components_dictionary, {})
-    
     def test_empty_graph(self):
         empty_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/empty_graph.dot'))
-        components = ConnectedComponents(empty_graph)
-        self.assertEqual(components.components_dictionary, {})
+        components = ConnectedComponents(empty_graph , empty_graph)
+        self.assertEqual(components.create_component_dictionary(), {})
     
-    def test_more_complicated_graph(self): #May want to change the asserts equal to look at objects
+    def test_component_dictionary(self): #May want to change the asserts equal to look at objects
         test_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/connected_component_graph.dot'))
-        components = ConnectedComponents(test_graph)
+        components = ConnectedComponents(test_graph , '')
         contig_component_1 = Component(0, sorted(['contig4', 'contig5', 'contig6', 'contig7']), sorted(['contig4', 'contig7']),\
                                          sorted([('contig4', 'contig5'), ('contig5', 'contig6'), ('contig6', 'contig7')]))
         contig_component_2 = Component(1, sorted(['contig2', 'contig3']), sorted(['contig2', 'contig3']), [('contig2', 'contig3')])
         contig_component_3 = Component(2, ['contig1'], ['contig1'], [])
-        component_dictionary = components.connected_components()
+        component_dictionary = components.create_component_dictionary()
         self.assertEqual(sorted(component_dictionary.keys()), sorted([0,1,2]))
         self.assertEqual(sorted(component_dictionary[0].contigs), sorted(['contig4', 'contig5', 'contig6', 'contig7']) )
         self.assertEqual(sorted(component_dictionary[0].ends), sorted(['contig4', 'contig7']))
@@ -33,3 +28,17 @@ class TestConnectedComponents(unittest.TestCase):
         self.assertEqual(sorted(component_dictionary[2].contigs), ['contig1'])
         self.assertEqual(sorted(component_dictionary[2].ends), ['contig1'])
         self.assertEqual(sorted(component_dictionary[2].edges), [])
+    
+    def test_contigs_to_components(self):
+        test_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/connected_component_graph.dot'))
+        components = ConnectedComponents(test_graph, test_graph)
+        expected_dictionary = {'contig1':2, 'contig2':1,'contig3':1, 'contig4':0, 'contig5':0, 'contig6':0, 'contig7':0}
+        self.assertDictEqual(components.create_contigs_to_components_dictionary(), expected_dictionary)
+    
+    def test_contigs_to_components_additional_contigs(self):
+        refined_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/connected_component_graph.dot'))
+        unrefined_graph = nx.Graph(nx.drawing.nx_pydot.read_dot('madansi/tests/data/connected_component_graph_extra_contigs.dot'))
+        components = ConnectedComponents(refined_graph, unrefined_graph)
+        expected_dictionary = {'contig1':2, 'contig2':1,'contig3':1, 'contig4':0, 'contig5':0, 'contig6':0, 'contig7':0, 'contig8':None, 'contig9':None}
+        self.assertDictEqual(components.create_contigs_to_components_dictionary(), expected_dictionary)
+        
