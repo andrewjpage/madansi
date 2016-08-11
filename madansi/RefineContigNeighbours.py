c840cd347c1119c2fa6f32ff1578d3d316513b85
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-11 14:19:30 +0100

    Updates to RefineContigNeighbours to consider the case when a contig is passed over in the expansion of the other contigs to find the connections

--- madansi/RefineContigNeighbours.py
+++ madansi/RefineContigNeighbours.py
@@ -11,35 +11,82 @@ class RefineContigNeighbours(object):
         self.filtered_blast_file = filtered_blast_file
         self.genes = GenesToContig(self.filtered_blast_file).genes_to_contig()
         self.refined_neighbouring_contigs = []
+        self.gene_detector = gene_detector
         self.contigs = gene_detector.contigs_to_genes()
+        self.components_of_contigs = {}
     
-    def refine_contig_neighbours(self):
-        for neighbours in self.neighbouring_contigs:
-            contig_appearances = self.find_contig_appearances(neighbours)
-            if  contig_appearances[neighbours[0][0]] in range(2, neighbours[1] + 4) and contig_appearances[neighbours[0][1]] in range(2, neighbours[1] + 4):
-                if len(contig_appearances)==2:
-                    self.refined_neighbouring_contigs.append(neighbours)     
+    def create_contig_max_iteration(self):
+        contig_count = 0
+        contig_max_iteration = {}
+        for contig in self.contigs:
+            contig_max_iteration[contig] = [contig_count, 0]
+            self.components_of_contigs[contig_count] = [contig]
+            contig_count += 1
+        return contig_max_iteration
+    
+    
+    def check_if_connection_is_valid(self, neighbours):
+        contig_appearances = self.find_contig_appearances(neighbours)
+        if  contig_appearances[neighbours[0][0]] in range(min(2, len(self.gene_detector.contigs_to_genes()[neighbours[0][0]].gene_objects)), neighbours[1] + 4) and\
+                         contig_appearances[neighbours[0][1]] in range(min(2, len(self.gene_detector.contigs_to_genes()[neighbours[0][1]].gene_objects)), neighbours[1] + 4):
+            if len(contig_appearances)==2:
+                return neighbours
             elif len(contig_appearances) == 3:
-                if len(self.contigs[neighbours[0][0]].gene_objects) in range(1, neighbours[1] + 2):
-                     if contig_appearances[neighbours[0][0]] == len(self.contigs[neighbours[0][0]].gene_objects):
-                         self.refined_neighbouring_contigs.append(neighbours)
-                elif len(self.contigs[neighbours[0][1]].gene_objects) in range(1, neighbours[1] + 2):
-                    if contig_appearances[neighbours[0][1]] == len(self.contigs[neighbours[0][1]].gene_objects):
-                        self.refined_neighbouring_contigs.append(neighbours)  
+                if len(self.gene_detector.contigs_to_genes()[neighbours[0][0]].gene_objects) in range(1, neighbours[1] + 2):
+                     if contig_appearances[neighbours[0][0]] == len(self.gene_detector.contigs_to_genes()[neighbours[0][0]].gene_objects):
+                        return neighbours
+                elif len(self.gene_detector.contigs_to_genes()[neighbours[0][1]].gene_objects) in range(1, neighbours[1] + 2):
+                    if contig_appearances[neighbours[0][1]] == len(self.gene_detector.contigs_to_genes()[neighbours[0][1]].gene_objects):
+                        return neighbours
+            
+    def refine_contig_neighbours(self):
+        contig_max_iteration = self.create_contig_max_iteration()
+        for neighbours in sorted(self.neighbouring_contigs, key= lambda x: x[1]):
+
+            if self.check_if_connection_is_valid(neighbours) != None:
+                neighbours = self.check_if_connection_is_valid(neighbours)
+                min_component_number = min(contig_max_iteration[neighbours[0][0]][0],contig_max_iteration[neighbours[0][1]][0])
+                max_component_number = max(contig_max_iteration[neighbours[0][0]][0],contig_max_iteration[neighbours[0][1]][0])
+                if min_component_number == max_component_number:
+                    if neighbours[1] <= contig_max_iteration[neighbours[0][0]][1] and neighbours[1] <= contig_max_iteration[neighbours[0][1]][1]:
+                        self.refined_neighbouring_contigs.append(neighbours)
+                        contigs_to_add = [contig for contig in [neighbours[0][0], neighbours[0][1]] if contig not in self.components_of_contigs[min_component_number]]
+                        for contig in contigs_to_add:
+                            self.components_of_contigs[min_component_number].append(contig)
+                else:
+                    self.refined_neighbouring_contigs.append(neighbours)
+                    if contig_max_iteration[neighbours[0][0]][0] == min_component_number:
+                        contig_max_iteration[neighbours[0][0]] = [min_component_number, neighbours[1]]
+                        contig_max_iteration[neighbours[0][1]] = [min_component_number, neighbours[1]]
+                        contigs_to_add = [contig for contig in self.components_of_contigs[max_component_number]]
+                        for contig in contigs_to_add:
+                            self.components_of_contigs[min_component_number].append(contig)
+                            contig_max_iteration[contig][0] = min_component_number
+                        self.components_of_contigs.pop(max_component_number)
+                    else:
+                        contig_max_iteration[neighbours[0][1]] = [min_component_number, neighbours[1]]
+                        contig_max_iteration[neighbours[0][0]] = [min_component_number, neighbours[1]]
+                        contigs_to_add = [contig for contig in self.components_of_contigs[max_component_number]]
+                        for contig in contigs_to_add:
+                            self.components_of_contigs[min_component_number].append(contig)
+                            contig_max_iteration[contig][0] = min_component_number
+                        self.components_of_contigs.pop(max_component_number)
         return self.refined_neighbouring_contigs
     
     def find_contig_appearances(self, neighbours):
         seen_nodes = []
         for intersection in neighbours[2]:
             seen_nodes.append(intersection)
-        if len(self.contigs[neighbours[0][0]].gene_objects) < neighbours[1] + 2 or len(self.contigs[neighbours[0][1]].gene_objects) < neighbours[1] + 2:
+        if len(self.gene_detector.contigs_to_genes()[neighbours[0][0]].gene_objects) <= neighbours[1] + 2 or len(self.gene_detector.contigs_to_genes()[neighbours[0][1]].gene_objects) <= neighbours[1] + 2:
             upper_limit = neighbours[1] + 1
         else:
             upper_limit = neighbours[1] + 2
+
         for i in range(upper_limit):
             neighbouring_nodes = NeighboursOfNodes(self.filtered_graph).find_neighbours(seen_nodes)
             for neighbour_node in neighbouring_nodes:
                 seen_nodes.append(neighbour_node)
+
         return self.count_contig_appearance(seen_nodes)
     
     def count_contig_appearance(self, gene_list):
--- madansi/tests/test_RefineContigNeighbours.py
+++ madansi/tests/test_RefineContigNeighbours.py
@@ -1,44 +1,16 @@
 import unittest
 import networkx as nx
 from madansi.RefineContigNeighbours import RefineContigNeighbours
+from madansi.GeneDetector import GeneDetector
 
 class TestRefineContigNeighbours(unittest.TestCase):
-    
-    def test_count_contig_appearances_empty_list_and_dictionary(self):
-        """Tests when both the gene list and the gene to contig dictionary is empty"""
-        refine_contig_neighbours = RefineContigNeighbours('','','madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {}
-        self.assertEqual(refine_contig_neighbours.count_contig_appearance([]), {})
-    
-    def test_count_contig_appearance_dictionary_empty(self):
-        """Tests when there is a gene in the gene list not given in the dictionary"""
-        refine_contig_neighbours = RefineContigNeighbours('','','madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {}
-        self.assertEqual(refine_contig_neighbours.count_contig_appearance(['gene1']), {})
-    
-    def test_count_contig_appearance_gene_list_empty(self):
-        """Tests when the dictionary is not empty but the gene list is"""
-        refine_contig_neighbours = RefineContigNeighbours('','','madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {'gene1':'Contig1'}
-        self.assertEqual(refine_contig_neighbours.count_contig_appearance([]), {})
-        
-    def test_count_contig_appearance_one_entry(self):
-        """Tests when the gene list has one entry present in the dictionary"""
-        refine_contig_neighbours = RefineContigNeighbours('','','madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2'}
-        self.assertEqual(refine_contig_neighbours.count_contig_appearance(['gene1']), {'Contig1':1})
-    
-    def test_count_contig_appearance_three_entries(self):
-        """Tests when there are multiple genes in the same contig present in the list"""
-        refine_contig_neighbours = RefineContigNeighbours('','','madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2'}
-        self.assertEqual(refine_contig_neighbours.count_contig_appearance(['gene1', 'gene2', 'gene3']), {'Contig1':2, 'Contig2':1})
-        
+       
     def test_keep_all_connections_empty_list(self):
         """Tests an empty graph and initial list of neighbouring contigs"""
         filtered_graph = nx.Graph()
         neighbouring_contigs = []
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file')
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/empty_file' )
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file',gene_detector)
         refine_contig_neighbours.genes = {}
         refine_contig_neighbours.refine_contig_neighbours()
         self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), sorted(neighbouring_contigs))
@@ -46,58 +18,130 @@ class TestRefineContigNeighbours(unittest.TestCase):
     def test_keep_one_connection(self):
         """Tests that one connection in neighbouring contigs is kept under refinement"""
         filtered_graph = nx.Graph()
-        filtered_graph.add_edges_from([('gene1', 'gene2'), ('gene2', 'gene5'), ('gene5', 'gene6'), ('gene6', 'gene3'), ('gene3', 'gene4')])
+        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene2', 'gene5'), ('gene5', 'gene6'),\
+                                        ('gene6', 'gene3'), ('gene3', 'gene4')])
         neighbouring_contigs = [[('Contig1', 'Contig2'), 2, ['gene5', 'gene6']]]
         
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file')
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/empty_file' )
+        
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file',gene_detector)
         refine_contig_neighbours.genes = {'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2', 'gene4':'Contig2'}
         refine_contig_neighbours.refine_contig_neighbours()
         self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), sorted(neighbouring_contigs))
     
-    def test_keep_three_connections(self):
-        """Tests that three connections are preserved under refinement"""
-        filtered_graph = nx.Graph()
-        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'), ('gene4', 'gene5'), ('gene5', 'gene6'), \
-                                        ('gene6', 'gene7'), ('gene7', 'gene8'), ('gene8', 'gene9'), ('gene9', 'gene10'), ('gene10', 'gene11')])
-        neighbouring_contigs = [[('Contig1', 'Contig2'),1, ['gene3', 'gene4'], [('Contig2', 'Contig3'),2,['gene6','gene7']], \
-                                [('Contig3', 'Contig4'),1,['gene9', 'gene10']]]]
-        
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1', 'gene4':'Contig2', 'gene5':'Contig2', \
-                                            'gene8':'Contig3', 'gene9':'Contig3', 'gene10':'Contig4', 'gene11':'Contig4'}
-        refine_contig_neighbours.refine_contig_neighbours()
-        self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), sorted(neighbouring_contigs))                        
-                                                                   
+    def test_keep_two_connections(self):
+       """Tests that two connections are preserved under refinement"""
+       filtered_graph = nx.Graph()
+       filtered_graph.add_edges_from([  ('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'),\
+                                        ('gene4', 'gene5'), ('gene5', 'gene6'), ('gene6', 'gene7'),\
+                                        ('gene7', 'gene8'), ('gene8', 'gene9'), ('gene9', 'gene10'),\
+                                        ('gene10', 'gene11'), ('gene11', 'gene12'), ('gene12', 'gene13')])
+                                        
+       neighbouring_contigs = [[('Contig1', 'Contig2'),1, ['gene4']], [('Contig2', 'Contig3'),2,['gene8','gene9']]]
+       gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/empty_file' )
+       
+       refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs,filtered_graph,'madansi/tests/data/empty_file', gene_detector)
+       refine_contig_neighbours.genes = {   'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1',\
+                                            'gene5':'Contig2', 'gene6':'Contig2', 'gene7':'Contig2', \
+                                            'gene10':'Contig3', 'gene11':'Contig3', 'gene11':'Contig3',\
+                                            'gene12':'Contig3'}
+       refine_contig_neighbours.refine_contig_neighbours()
+       self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), sorted(neighbouring_contigs))                        
+                                                                  
     def test_three_contigs_together(self):
         """Tests when there are three contigs within a small distance of the intersection points"""
         filtered_graph = nx.Graph()
-        filtered_graph.add_edges_from([('gene1', 'gene2'), ('gene3', 'gene4'), ('gene5', 'gene6'), ('gene7','gene2'), ('gene7','gene3'), ('gene7', 'gene5')])
-        neighbouring_contigs = [[('Contig1', 'Contig2'),1,['gene7']], [('Contig2', 'Contig3'),1,['gene7']], [('Contig1', 'Contig3'),1,['gene7']]]
+        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene3', 'gene4'), ('gene5', 'gene6'),\
+                                        ('gene7','gene2'), ('gene7','gene3'), ('gene7', 'gene5')])
+                                        
+        neighbouring_contigs =  [[('Contig1', 'Contig2'),1,['gene7']], [('Contig2', 'Contig3'),1,['gene7']],\
+                                [('Contig1', 'Contig3'),1,['gene7']]]
+                                
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/empty_file' )
         
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = { 'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2', 'gene4':'Contig2', 'gene5':'Contig3', 'gene6':'Contig3'}
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/empty_file',gene_detector)
+        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2',\
+                                            'gene4':'Contig2', 'gene5':'Contig3', 'gene6':'Contig3'}
         refine_contig_neighbours.refine_contig_neighbours()
         self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), [])
-        
+       
     def test_single_gene_from_a_contig(self):
-        """Tests when there are two contigs present, one with a single gene on it"""
+        """Tests when there are two contigs present, one with a single gene on it when other genes from the same contig are elsewhere in the graph"""
         filtered_graph = nx.Graph()
-        filtered_graph.add_edges_from([('gene1', 'gene2'), ('gene2', 'gene3')])
+        filtered_graph.add_edges_from([('gene1', 'gene2'), ('gene2', 'gene3'), ('gene4', 'gene5')])
         neighbouring_contigs = [[('Contig1', 'Contig2'),1,['gene2', 'gene3']]]
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/refine_contig_neighbours_5_blast_hits_file' )
         
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = { 'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2'}
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/refine_contig_neighbours_5_blast_hits_file',gene_detector)
+        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig2',\
+                                            'gene4':'Contig2', 'gene5':'Contig2'}
         refine_contig_neighbours.refine_contig_neighbours()
         self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), [])
     
     def test_contig_joins_in_middle(self):
         """Tests when an intersection is found in the case where one end of the contig is closest to the middle of a second"""
         filtered_graph = nx.Graph()
-        filtered_graph.add_edges_from([('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'), ('gene4', 'gene5'), ('gene2', 'gene6'), ('gene6', 'gene7')])
-        neighbouring_contigs = [[('Contig1', 'Contig2'),1,['gene2', 'gene6']]]
+        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'),\
+                                        ('gene4', 'gene5'), ('gene5', 'gene6') ,('gene3', 'gene7'),\
+                                        ('gene7', 'gene8')])
+                                        
+        neighbouring_contigs = [[('Contig1', 'Contig2'),1,['gene3', 'gene7']]]
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/empty_file' )
         
-        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/empty_file')
-        refine_contig_neighbours.genes = { 'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1', 'gene4':'Contig1', 'gene5':'Contig1', 'gene6':'Contig2', 'gene7':'Contig2'}
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/empty_file',gene_detector)
+        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1', 'gene4':'Contig1',\
+                                            'gene5':'Contig1', 'gene6':'Contig1', 'gene7':'Contig2', 'gene8':'Contig2'}
         refine_contig_neighbours.refine_contig_neighbours()
         self.assertEqual(sorted(refine_contig_neighbours.refined_neighbouring_contigs), [])
+    
+    def test_short_contig_in_middle(self):
+        """Tests the case when there is a contig with only two genes between longer ones, we should find that the connection that needs more iterations is removed"""
+        filtered_graph = nx.Graph()
+        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'),\
+                                        ('gene4', 'gene5'), ('gene5', 'gene6'), ('gene6', 'gene7'),\
+                                        ('gene7', 'gene8')])
+                                        
+        neighbouring_contigs = [[('Contig1', 'Contig2'),1,['gene3','gene4']],\
+                                [('Contig2', 'Contig3'),1,['gene5', 'gene6']],\
+                                [('Contig1', 'Contig3'),2,['gene4', 'gene5']]]
+                                
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/refine_contig_neighbours_8_blast_hits_file' )
+        
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/refine_contig_neighbours_8_blast_hits_file', gene_detector)
+        refine_contig_neighbours.contigs = {'Contig1': {'gene1':None, 'gene2':None, 'gene3':None},\
+                                            'Contig2':{'gene4':None, 'gene5':None},\
+                                            'Contig3':{'gene6':None, 'gene7':None, 'gene8':None}}
+                                            
+        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1',\
+                                            'gene4':'Contig2', 'gene5':'Contig2', 'gene6':'Contig3',\
+                                            'gene7':'Contig3', 'gene8':'Contig3'}
+    
+        self.assertEqual(sorted(refine_contig_neighbours.refine_contig_neighbours()), sorted([[('Contig1', 'Contig2'),1,['gene3', 'gene4']], [('Contig2', 'Contig3'),1,['gene5', 'gene6']]]))
+    
+    def test_keep_three_cycle_two_equal_weights(self):
+        """Tests that when there are three cycles with all connections having equal weight, these are preserved under refinement"""
+        filtered_graph = nx.Graph()
+        filtered_graph.add_edges_from([ ('gene1', 'gene2'), ('gene2', 'gene3'), ('gene3', 'gene4'),\
+                                        ('gene4', 'gene5'), ('gene6', 'gene7'), ('gene7', 'gene8'),\
+                                        ('gene8', 'gene1')])
+        
+        neighbouring_contigs = [[('Contig1', 'Contig2'), 1, ['gene3', 'gene4']],\
+                                [('Contig2', 'Contig3'), 1, ['gene5', 'gene6']],\
+                                [('Contig1', 'Contig3'), 1, ['gene8', 'gene1']]]
+        
+        gene_detector = GeneDetector('madansi/tests/data/assembly_4_sequences.fa', 'madansi/tests/data/refine_contig_neighbours_8_blast_hits_file')
+        
+        
+        refine_contig_neighbours = RefineContigNeighbours(neighbouring_contigs, filtered_graph, 'madansi/tests/data/refine_contig_neighbours_8_blast_hits_file', gene_detector)
+        
+        refine_contig_neighbours.contigs = {'Contig1': {'gene1':None, 'gene2':None, 'gene3':None},\
+                                            'Contig2':{'gene4':None, 'gene5':None},\
+                                            'Contig3':{'gene6':None, 'gene7':None, 'gene8':None}}
+                                            
+        refine_contig_neighbours.genes = {  'gene1':'Contig1', 'gene2':'Contig1', 'gene3':'Contig1',\
+                                            'gene4':'Contig2', 'gene5':'Contig2', 'gene6':'Contig3',\
+                                            'gene7':'Contig3', 'gene8':'Contig3'}
+        
+        self.assertEqual(sorted(refine_contig_neighbours.refine_contig_neighbours()), sorted(neighbouring_contigs))
+        
     
