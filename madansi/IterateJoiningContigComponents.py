c8417d43cd1324544445fb367b5a8bdcc43c30fd
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-11 17:05:16 +0100

    Renamed variable from output graph to output refined graph

--- madansi/IterateJoiningContigComponents.py
+++ madansi/IterateJoiningContigComponents.py
@@ -3,17 +3,18 @@ from madansi.JoiningContigComponents import JoiningContigComponents
 
 class IterateJoiningContigComponents(object):
     
-    def __init__(self, unrefined_graph, output_graph):
+    def __init__(self, unrefined_graph, output_refined_graph):
         self.temp_graph = nx.Graph()
         self.unrefined_graph = unrefined_graph
-        self.output_graph = output_graph
+        self.output_refined_graph = output_refined_graph
     
     def iterate_joining(self, ordered_contig_graph):
         while not nx.is_isomorphic(self.temp_graph, ordered_contig_graph):
-            self.temp_graph = ordered_contig_graph
-            join_components = JoiningContigComponents(ordered_contig_graph, self.unrefined_graph)
-            ordered_contig_graph = join_components.add_edges()
+            self.temp_graph         = ordered_contig_graph
+            join_components         = JoiningContigComponents(ordered_contig_graph, self.unrefined_graph)
+            ordered_contig_graph    = join_components.add_edges()
         return ordered_contig_graph
     
     def output_graph(self, graph):
-        nx.drawing.nx_pydot.write_dot(graph, self.output_graph)
+        nx.drawing.nx_pydot.write_dot(graph, self.output_refined_graph)
+        
