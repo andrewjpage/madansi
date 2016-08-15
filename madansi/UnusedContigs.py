c49bf9af25486f03a7a3eb116e4d6fbc20a2f67a
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-15 15:39:07 +0100

    Added the method to find unused contigs from the graph

--- madansi/UnusedContigs.py
+++ madansi/UnusedContigs.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 class UnusedContigs(object):
     def __init__(self,gene_detector, sequence_list, unused_contigs_file): 
         self.gene_detector = gene_detector
@@ -18,3 +20,12 @@ class UnusedContigs(object):
             f.write(unused_contig + '\n')
         f.close()
         
+    def contigs_not_in_refined_graph(self,refined_graph):
+        contigs_present_in_refined_graph = refined_graph.nodes()
+        for contig in self.sequence_list:
+            if  contig  not in contigs_present_in_refined_graph and \
+                contig  not in self.unused_contigs_list:
+                self.unused_contigs_list.append(contig)
+        return self.unused_contigs_list
+        
+    
