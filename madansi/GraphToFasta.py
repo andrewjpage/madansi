57cc274b1b545ec059df90290104eb830fec8383
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-16 14:02:17 +0100

    Tests for writing to a fasta file

--- madansi/GraphToFasta.py
+++ madansi/GraphToFasta.py
@@ -1,48 +1,84 @@
 import networkx as nx
 from madansi.Assembly import Assembly
+import pprint
 
 class GraphToFasta(object):
     
-    def __init__(self,input_fasta_file, graph, output_fasta_fname):
+    def __init__(self,input_fasta_file, graph, output_fasta_fname, contig_ends):
         self.input_fasta_file = input_fasta_file
         self.graph = graph
         self.output_fasta_fname = output_fasta_fname
+        self.contig_ends = contig_ends
+        self.contig_orientation = {}
 
     def find_sequences(self):
         assembly = Assembly(self.input_fasta_file)
         assembly.sequence_names()
         return assembly.sequences
         
-    def find_start_contig(self, component):
-
+    def find_contigs_degree_one(self, component):
+        contigs_degree_one = []
         for contig in component:
             if self.graph.degree(contig) == 1:
-                return contig
+                contigs_degree_one.append(contig)
+        return contigs_degree_one
     
+    def determine_orientation(self, visited, contig):
+        
+        if len(self.contig_ends[contig]) == 2:
+            for neighbour in self.graph.neighbors(contig):
+                if neighbour in visited:
+                    visited_contig = neighbour
+                else:
+                    unvisited_contig = neighbour
+            if self.contig_ends[contig][visited_contig] <= self.contig_ends[contig][unvisited_contig]:
+                self.contig_orientation[contig] = 1
+            else:
+                self.contig_orientation[contig] = -1
+        else:
+            
+            
+            self.contig_orientation[contig] = 1
+        return self.contig_orientation[contig]
+
     def walk_contig_graph(self, component):
         
-        contig = self.find_start_contig(component)
+        contigs_degree_one = self.find_contigs_degree_one(component)
+        contig = sorted(contigs_degree_one)[0]
+        self.contig_orientation[contig] = 1
         visited = [contig]
+        ends_seen = [contig]
         
         while self.graph.degree(contig)<= 2 and sorted(visited) != sorted(component):
             for neighbour in self.graph.neighbors(contig):
                 if neighbour not in visited:
+                    self.determine_orientation(visited, neighbour)
                     visited.append(neighbour)
                     contig = neighbour
+
         return visited
     
     def create_fasta_file(self):
+        
         sequences = self.find_sequences()
         f = open(self.output_fasta_fname, 'w')
         for component in sorted(nx.connected_components(self.graph), key = len, reverse=True):
             visited = self.walk_contig_graph(list(component))
+            pprint.pprint(self.contig_orientation)
             for contig in visited:
-                f.write('> ' + contig + '\n')
-                f.write(str(sequences[contig]) + '\n')
-                print('added ' + contig)
-                
+                f.write('>' + contig + '\n')
+                if self.contig_orientation[contig] == 1:
+                    f.write(str(sequences[contig][0]) + '\n')
+                else:
+                    f.write(str(sequences[contig][1]) + '\n')
         f.close()
         
+    def combine_contigs(self, split_components):
+        sequences = self.find_sequences()
+        for split_component in split_components:
+            pass
+            
+            
 
             
     
--- madansi/tests/test_GraphToFasta.py
+++ madansi/tests/test_GraphToFasta.py
@@ -2,13 +2,40 @@ import unittest
 from madansi.GraphToFasta import GraphToFasta
 import os
 import filecmp
+import networkx as nx
+
 
 class TestGraphToFasta(unittest.TestCase):
     
-    def test_empty_graph(self):
-        """Should return an empty fasta file"""
+    def test_find_contigs_degree_one(self):
         graph = nx.Graph()
-        fname = "output.fa"
-        graph_to_fasta_object = GraphToFasta(graph, fname)
-        self.assertTrue(filecmp.cmp(graph_to_fasta_object.output_file(), 'madansi/tests/data/empty_file.fa'))
-        os.unlink(fname)
+        graph.add_edges_from([('Contig1', 'Contig2'), ('Contig2', 'Contig3'), ('Contig3', 'Contig4')])
+        graph_to_fasta = GraphToFasta('', graph, '', {})
+        self.assertEqual(sorted(['Contig1', 'Contig4']), sorted(graph_to_fasta.find_contigs_degree_one(['Contig1', 'Contig2', 'Contig3', 'Contig4'])))
+    
+    def test_determine_orientation(self):
+        graph = nx.Graph()
+        graph.add_edges_from([('Contig1', 'Contig2'), ('Contig2', 'Contig3'), ('Contig3', 'Contig4')])
+        contig_ends = {'Contig1':{'Contig2': 1}, 'Contig2':{'Contig1':29, 'Contig3':307}, 'Contig3':{'Contig2':1000, 'Contig4':201}, 'Contig4':{'Contig3':49}}
+        graph_to_fasta = GraphToFasta('', graph, '', contig_ends)
+        self.assertEqual(graph_to_fasta.determine_orientation(['Contig1', 'Contig2'], 'Contig3'), -1)
+        self.assertEqual(graph_to_fasta.determine_orientation(['Contig1'], 'Contig2'), 1)
+        self.assertEqual(graph_to_fasta.determine_orientation([], 'Contig1'), 1)
+        self.assertEqual(graph_to_fasta.determine_orientation(['Contig1', 'Contig2', 'Contig3'], 'Contig4'), 1)
+    
+    def test_walk_contig_graph(self):
+        graph = nx.Graph()
+        graph.add_edges_from([('Contig1', 'Contig2'), ('Contig2', 'Contig3'), ('Contig3', 'Contig4')])
+        contig_ends = {'Contig1':{'Contig2': 1}, 'Contig2':{'Contig1':29, 'Contig3':307}, 'Contig3':{'Contig2':1000, 'Contig4':201}, 'Contig4':{'Contig3':49}}
+        graph_to_fasta = GraphToFasta('', graph, '', contig_ends)
+        self.assertEqual(['Contig1', 'Contig2', 'Contig3', 'Contig4'], graph_to_fasta.walk_contig_graph(['Contig1', 'Contig2', 'Contig3', 'Contig4']))
+    
+    def test_create_fasta_file(self):
+        graph = nx.Graph()
+        graph.add_edges_from([('Contig1', 'Contig3'), ('Contig3', 'Contig2'), ('Contig2', 'Contig4')])
+        contig_ends = {'Contig1':{'Contig3': 1}, 'Contig3':{'Contig1':29, 'Contig2':307}, 'Contig2':{'Contig3':1000, 'Contig4':201}, 'Contig4':{'Contig2':49}}
+        graph_to_fasta = GraphToFasta('madansi/tests/data/assembly_4_sequences.fa', graph, 'output.fa', contig_ends)
+        graph_to_fasta.create_fasta_file()
+        self.assertTrue(filecmp.cmp('madansi/tests/data/expected_ordered_contigs.fa', 'output.fa'))
+        
+        
