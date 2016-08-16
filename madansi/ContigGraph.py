52ffd351333a85e466c78898c6a5c58d11b1fe66
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-16 14:02:36 +0100

    Remove weight

--- madansi/ContigGraph.py
+++ madansi/ContigGraph.py
@@ -1,17 +1,17 @@
 import networkx as nx
+import pprint
 
 class ContigGraph(object):
     """Given a list of tuples of pairs of contigs together with the distance between them, will generate a weighted graph showing all\
     of these connections together with the distance between the two contigs."""
     
-    def __init__(self, neighbouring_contigs):
-        self.neighbouring_contigs = neighbouring_contigs
+    def __init__(self, contig_connections):
+        self.contig_connections = contig_connections
         self.contig_graph = nx.Graph()
     
     def create_contig_subgraph(self):
-        for entry in self.neighbouring_contigs:
-            self.contig_graph.add_nodes_from([entry[0][0], entry[0][1]])
-            self.contig_graph.add_edge(entry[0][0],entry[0][1], weight = entry[1])
+        for entry in self.contig_connections:
+            self.contig_graph.add_edge(entry[0][0],entry[0][1])
         return self.contig_graph
     
         
