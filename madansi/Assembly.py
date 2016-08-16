1f3a2dd355b54b4dca7cf0c23240e1ad51a08204
Joanna Tumelty <jt20@loan106451.internal.sanger.ac.uk>
2016-08-16 08:31:30 +0100

    Added ability to store sequence data.

--- madansi/Assembly.py
+++ madansi/Assembly.py
@@ -4,12 +4,14 @@ class Assembly(object):
     """Gives a list of all the contigs present in the sample"""
     def __init__(self, input_file):
         self.input_file = input_file
+        self.sequences = {}
     
     def sequence_names(self):
         sequence_list = []
         with open(self.input_file, "r") as handle:
             for seq_record in SeqIO.parse( handle, "fasta"):
                 sequence_list.append(seq_record.id)
+                self.sequences[seq_record.id] = [seq_record.seq, seq_record.reverse_complement()]
             handle.close()
         return sequence_list
        
